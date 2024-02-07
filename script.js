7723579bf8f7401b711202e2b6366b38d0046a13
olawuyi tofunmi samuel <126236665+tosdev06@users.noreply.github.com>
2024-02-07 14:09:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// You can add JavaScript functionality here if needed
+// For example, to make the navigation smooth-scrolling:
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
